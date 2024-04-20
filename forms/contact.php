5fd2139afff07ac243221acafcca8a82603f6fce
Tamemroofa <151266843+Tamemroofa@users.noreply.github.com>
2024-04-20 17:00:10 +0100

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -23,8 +23,6 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
   $contact->smtp = array(
     'host' => 'smtp-mail.outlook.com',
     'username' => 'Tamem@roofa.co',
