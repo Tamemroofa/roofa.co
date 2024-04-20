9bb89e1ea2da03dc6a1b348db9d0e21a5e269f19
Tamemroofa <151266843+Tamemroofa@users.noreply.github.com>
2024-04-20 16:58:17 +0100

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -26,12 +26,12 @@
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp-mail.outlook.com',
+    'username' => 'Tamem@roofa.co',
+    'password' => 'Ahsanullah nawazai786',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
